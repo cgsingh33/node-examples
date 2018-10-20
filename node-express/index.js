67c3eab8c2c29a539e7157be5886fb3e1e423da1
chandragupta singh <chandragupta.singh2016@vitstudent.ac.in>
2018-10-21 00:33:49 +0530

    first assignment

--- node-express/index.js
+++ node-express/index.js
@@ -2,16 +2,21 @@ const express = require('express');
 const http = require('http');
 const morgan =require('morgan');
 const bodyParser = require('body-parser');
+
 const dishRouter= require('./routes/dishRouter');
+const promoRouter= require('./routes/promoRouter');
+const leaderRouter= require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express(); // whenever use a middlewere write app.use()
 app.use(morgan('dev')); // print out additional information to the screen
-app.use(bodyParser.json());
-app.use('/dishes', dishRouter);
+app.use(bodyParser.json()); //morgan, bodyParser, dishRouter are middleware 
 
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname + '/public')); // to access static html file from public folder
 
